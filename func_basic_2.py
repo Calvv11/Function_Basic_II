d6d3d08c8185098ac5ba0985f9983770de22f06c
Calvv11 <thedonutdog125@gmail.com>
2022-04-09 01:59:46 -0700

    Initial Commit

---
+++ func_basic_2.py
@@ -0,0 +1,45 @@
+# Countdown
+# def countdown(num):
+#     output = []
+#     for i in range(num, -1, -1):
+#         output.append(i)
+#     return output
+
+# print(countdown(15))
+
+# Print and Return
+# def print_and_return(list):
+#     print(list[0])
+#     return list[1]
+
+# print(print_and_return([3,4]))
+
+# First Plus Length
+# def first_plus_length(list):
+#     return list[0] + len(list)
+
+# print(first_plus_length([10,20,30,40,50,60]))
+
+# Values Greater Than Second
+# def vals_greater_than_second(list):
+#     output = []
+#     if len(list) < 2:
+#         return False
+#     for i in range(0, len(list)):
+#         if list[i] > list[1]:
+#             output.append(list[i])
+#     print(len(output))
+#     return output
+
+# print(vals_greater_than_second([10,3,30,200,31,99]))
+# print(vals_greater_than_second([5]))
+
+# This Length, That Value
+# def this_length_that_value(size, value):
+#     output = []
+#     for i in range(0, size):
+#         output.append(value)
+#     return output
+
+# print(this_length_that_value(3,5))
+# print(this_length_that_value(5,7))
